79e378628bdbc04842db18850f4de511af3f3eb2
NoeOnDev <alxg5516@gmail.com>
2024-06-06 11:15:20 -0600

    feat: Add dataset and store input variables and desired output

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
+# Aquí cargo el dataset
 dataset = pd.read_excel('dataset.xlsx')
 
+# Aquí almaceno las variables de entrada para cada fila
 x1 = dataset['x1'].tolist()
 x2 = dataset['x2'].tolist()
 x3 = dataset['x3'].tolist()
 x4 = dataset['x4'].tolist()
-y = dataset['y'].tolist()
+# Aquí almaceno la "y" deseada para cada fila
+yd = dataset['y'].tolist()
