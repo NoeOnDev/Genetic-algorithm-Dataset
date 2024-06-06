4b786d68f22ee1dc6ccc2d390d3e8263952285e9
NoeOnDev <alxg5516@gmail.com>
2024-06-06 11:58:22 -0600

    Refactorizar almacenamiento de variables de entrada en listas

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import pandas as pd
 dataset = pd.read_excel('dataset.xlsx')
 
 # Aquí almaceno las variables de entrada para cada fila
-x1 = dataset['x1'].tolist()
-x2 = dataset['x2'].tolist()
-x3 = dataset['x3'].tolist()
-x4 = dataset['x4'].tolist()
+x1 = dataset['x1'].tolist() # Convierto la columna x1 en una lista 31 elementos
+x2 = dataset['x2'].tolist() # Convierto la columna x2 en una lista 31 elementos
+x3 = dataset['x3'].tolist() # Convierto la columna x3 en una lista 31 elementos
+x4 = dataset['x4'].tolist() # Convierto la columna x4 en una lista 31 elementos
+
 # Aquí almaceno la "y" deseada para cada fila
 yd = dataset['y'].tolist()
+
