5794cea25bed5822b4a36fc2bc9d13d2553e940a
NoeOnDev <alxg5516@gmail.com>
2024-06-06 11:13:26 -0600

    feat: Add columns x1, x2, x3, x4, and y to dataset

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 dataset = pd.read_excel('dataset.xlsx')
 
-print(dataset)
+x1 = dataset['x1'].tolist()
+x2 = dataset['x2'].tolist()
+x3 = dataset['x3'].tolist()
+x4 = dataset['x4'].tolist()
+y = dataset['y'].tolist()
