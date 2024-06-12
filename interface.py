98cf3325a07546ada479670c9a0100a90a7137ba
NoeOnDev <alxg5516@gmail.com>
2024-06-12 03:55:29 -0600

    refactor: Mejorar legibilidad y usabilidad de la interfaz gráfica

---
+++ interface.py
@@ -0,0 +1,60 @@
+from tkinter import Tk, Label, Entry, Button, ttk
+from main import algoritmo_genetico
+
+def mostrar_tabla(mejores):
+    for item in treeview.get_children():
+        treeview.delete(item)
+    for mejor in mejores:
+        treeview.insert("", "end", values=(mejor['Generacion'], mejor['fitness'], mejor['error'], ':'.join(map(str, mejor['constantes']))))
+
+def iniciar_algoritmo():
+    algoritmo_genetico(p_mutacion.get(), p_mutaciong.get(), n_generaciones.get(), poblacion_max.get(), poblacion_min.get(), mostrar_tabla)
+
+def mostrar_ventana():
+    global ventana, p_mutacion, p_mutaciong, n_generaciones, poblacion_max, poblacion_min, treeview
+    ventana = Tk()
+    ventana.title("Algoritmo Genético individual 8A")
+
+    estilo = ttk.Style()
+    estilo.configure("TLabel", font=("Arial", 14))
+    estilo.configure("TButton", font=("Arial", 14))
+    estilo.configure("TEntry", font=("Arial", 14))
+    estilo.configure("Treeview.Heading", font=("Arial", 14, "bold"))
+    estilo.configure("Treeview", font=("Arial", 12))
+
+    Label(ventana, text="Población mínima:", font=("Arial", 14)).grid(row=1, column=0, padx=10, pady=10)
+    Label(ventana, text="Población máxima:", font=("Arial", 14)).grid(row=2, column=0, padx=10, pady=10)
+    Label(ventana, text="Probabilidad de mutación del individuo:", font=("Arial", 14)).grid(row=3, column=0, padx=10, pady=10)
+    Label(ventana, text="Probabilidad de mutación del gen:", font=("Arial", 14)).grid(row=4, column=0, padx=10, pady=10)
+    Label(ventana, text="Generaciones:", font=("Arial", 14)).grid(row=5, column=0, padx=10, pady=10)
+
+    poblacion_min = Entry(ventana, font=("Arial", 14))
+    poblacion_max = Entry(ventana, font=("Arial", 14))
+    p_mutacion = Entry(ventana, font=("Arial", 14))
+    p_mutaciong = Entry(ventana, font=("Arial", 14))
+    n_generaciones = Entry(ventana, font=("Arial", 14))
+
+    poblacion_min.grid(row=1, column=1, padx=10, pady=10)
+    poblacion_max.grid(row=2, column=1, padx=10, pady=10)
+    p_mutacion.grid(row=3, column=1, padx=10, pady=10)
+    p_mutaciong.grid(row=4, column=1, padx=10, pady=10)
+    n_generaciones.grid(row=5, column=1, padx=10, pady=10)
+
+    Button(ventana, text="Iniciar", command=iniciar_algoritmo, font=("Arial", 14)).grid(row=6, column=0, columnspan=3, pady=20)
+
+    treeview = ttk.Treeview(ventana, columns=("Generación", "Fitness", "Error", "Constantes"), show="headings", height=20, selectmode="browse")
+    treeview.heading("Generación", text='Generación')
+    treeview.heading("Fitness", text="Fitness")
+    treeview.heading("Error", text="Error")
+    treeview.heading("Constantes", text="Constantes")
+    treeview.column("Generación", width=150)
+    treeview.column("Fitness", width=200)
+    treeview.column("Error", width=200)
+    treeview.column("Constantes", width=800)
+    treeview.grid(row=7, column=0, columnspan=3, padx=0, pady=20)
+
+    ventana.geometry("1350x765")
+    ventana.mainloop()
+
+if __name__ == "__main__":
+    mostrar_ventana()
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import random
 import numpy as np
-from tkinter import Tk, Label, Entry, Button, ttk
 from graphics import crear_grafica, crear_grafica_error, crear_graficas_constante, crear_video
 
 dataset = pd.read_excel('2024.05.22 dataset 8A.xlsx')
@@ -25,13 +24,44 @@ def calcular_error(y_deseada, y_calculada):
 def calcular_norma_error(error):
     return np.linalg.norm(error)
 
-# Algoritmo Genético
-def algoritmo_genetico():
-    probabilidad_mutacion_individual = float(p_mutacion.get())
-    probabilidad_mutacion_gen = float(p_mutaciong.get())
-    cantidad_generaciones = int(n_generaciones.get())
-    poblacion_maxima = int(poblacion_max.get())
-    poblacion_minima = int(poblacion_min.get())
+def cruza(pareja1, pareja2):
+    posicion = random.randint(1, len(pareja1) - 1)
+    hijo1 = pareja1[:posicion] + pareja2[posicion:]
+    hijo2 = pareja2[:posicion] + pareja1[posicion:]
+    return hijo1, hijo2
+
+def mutacion(individuo, probabilidad_mutacion_gen):
+    nuevo = individuo.copy()
+    muto = False
+    while not muto:
+        for i, constante in enumerate(nuevo):
+            if random.random() < probabilidad_mutacion_gen:
+                nuevo[i] = constante * (1.0 + random.uniform(-100, 100) / 1000)
+                muto = True
+    return nuevo
+
+def definir_mutacion(hijo, probabilidad_mutacion_individual, probabilidad_mutacion_gen):
+    if random.random() < probabilidad_mutacion_individual:
+        hijo = mutacion(hijo, probabilidad_mutacion_gen)
+    return hijo
+
+def podar(poblacion, poblacion_maxima):
+    return poblacion[:poblacion_maxima]
+
+def generar_parejas(poblacion):
+    parejas_cruce = []
+    for i in range(len(poblacion)):
+        cantidad_parejas = random.randint(1, len(poblacion) - 1)
+        parejas = random.sample(poblacion[:i] + poblacion[i + 1:], cantidad_parejas)
+        parejas_cruce.append((poblacion[i], parejas))
+    return parejas_cruce
+
+def algoritmo_genetico(p_mutacion, p_mutaciong, n_generaciones, poblacion_max, poblacion_min, actualizar_tabla):
+    probabilidad_mutacion_individual = float(p_mutacion)
+    probabilidad_mutacion_gen = float(p_mutaciong)
+    cantidad_generaciones = int(n_generaciones)
+    poblacion_maxima = int(poblacion_max)
+    poblacion_minima = int(poblacion_min)
     individuos_iniciales = random.randint(poblacion_minima, poblacion_maxima)
     
     poblacion = [generar_constantes() for _ in range(individuos_iniciales)]
@@ -65,96 +95,8 @@ def algoritmo_genetico():
         poblacion = podar(nueva_poblacion, poblacion_maxima)
         generaciones.append(poblacion)
 
-    mostrar_tabla(mejores)
+    actualizar_tabla(mejores)
     crear_grafica_error(errores_menores, promedio_errores, peores)
     a, b, c, d, e = zip(*[mejor['constantes'] for mejor in mejores])
     crear_graficas_constante(a, b, c, d, e)
     crear_video()
-
-# Funciones genéticas
-def cruza(pareja1, pareja2):
-    posicion = random.randint(1, len(pareja1) - 1)
-    hijo1 = pareja1[:posicion] + pareja2[posicion:]
-    hijo2 = pareja2[:posicion] + pareja1[posicion:]
-    return hijo1, hijo2
-
-def mutacion(individuo, probabilidad_mutacion_gen):
-    nuevo = individuo.copy()
-    muto = False
-    while not muto:
-        for i, constante in enumerate(nuevo):
-            if random.random() < probabilidad_mutacion_gen:
-                nuevo[i] = constante * (1.0 + random.uniform(-100, 100) / 1000)
-                muto = True
-    return nuevo
-
-def definir_mutacion(hijo, probabilidad_mutacion_individual, probabilidad_mutacion_gen):
-    if random.random() < probabilidad_mutacion_individual:
-        hijo = mutacion(hijo, probabilidad_mutacion_gen)
-    return hijo
-
-def podar(poblacion, poblacion_maxima):
-    return poblacion[:poblacion_maxima]
-
-def generar_parejas(poblacion):
-    parejas_cruce = []
-    for i in range(len(poblacion)):
-        cantidad_parejas = random.randint(1, len(poblacion) - 1)
-        parejas = random.sample(poblacion[:i] + poblacion[i + 1:], cantidad_parejas)
-        parejas_cruce.append((poblacion[i], parejas))
-    return parejas_cruce
-
-def mostrar_tabla(mejores):
-    for item in treeview.get_children():
-        treeview.delete(item)
-    for mejor in mejores:
-        treeview.insert("", "end", values=(mejor['Generacion'], mejor['fitness'], mejor['error'], ':'.join(map(str, mejor['constantes']))))
-
-# Interfaz gráfica
-def mostrar_ventana():
-    global ventana, p_mutacion, p_mutaciong, n_generaciones, poblacion_max, poblacion_min, treeview
-    ventana = Tk()
-    ventana.title("Algoritmo Genético individual 8A")
-
-    estilo = ttk.Style()
-    estilo.configure("TLabel", font=("Arial", 14))
-    estilo.configure("TButton", font=("Arial", 14))
-    estilo.configure("TEntry", font=("Arial", 14))
-    estilo.configure("Treeview.Heading", font=("Arial", 14, "bold"))
-    estilo.configure("Treeview", font=("Arial", 12))
-
-    Label(ventana, text="Población mínima:", font=("Arial", 14)).grid(row=1, column=0, padx=10, pady=10)
-    Label(ventana, text="Población máxima:", font=("Arial", 14)).grid(row=2, column=0, padx=10, pady=10)
-    Label(ventana, text="Probabilidad de mutación del individuo:", font=("Arial", 14)).grid(row=3, column=0, padx=10, pady=10)
-    Label(ventana, text="Probabilidad de mutación del gen:", font=("Arial", 14)).grid(row=4, column=0, padx=10, pady=10)
-    Label(ventana, text="Generaciones:", font=("Arial", 14)).grid(row=5, column=0, padx=10, pady=10)
-
-    poblacion_min = Entry(ventana, font=("Arial", 14))
-    poblacion_max = Entry(ventana, font=("Arial", 14))
-    p_mutacion = Entry(ventana, font=("Arial", 14))
-    p_mutaciong = Entry(ventana, font=("Arial", 14))
-    n_generaciones = Entry(ventana, font=("Arial", 14))
-
-    poblacion_min.grid(row=1, column=1, padx=10, pady=10)
-    poblacion_max.grid(row=2, column=1, padx=10, pady=10)
-    p_mutacion.grid(row=3, column=1, padx=10, pady=10)
-    p_mutaciong.grid(row=4, column=1, padx=10, pady=10)
-    n_generaciones.grid(row=5, column=1, padx=10, pady=10)
-
-    Button(ventana, text="Iniciar", command=algoritmo_genetico, font=("Arial", 14)).grid(row=6, column=0, columnspan=3, pady=20)
-
-    treeview = ttk.Treeview(ventana, columns=("Generación", "Fitness", "Error", "Constantes"), show="headings", height=20, selectmode="browse")
-    treeview.heading("Generación", text='Generación')
-    treeview.heading("Fitness", text="Fitness")
-    treeview.heading("Error", text="Error")
-    treeview.heading("Constantes", text="Constantes")
-    treeview.column("Generación", width=150)
-    treeview.column("Fitness", width=200)
-    treeview.column("Error", width=200)
-    treeview.column("Constantes", width=800)
-    treeview.grid(row=7, column=0, columnspan=3, padx=0, pady=20)
-
-    ventana.geometry("1350x765")
-    ventana.mainloop()
-
-mostrar_ventana()
